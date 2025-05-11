6a0885ef842f400f9a0742181a80e588c573e698
Mohammad Azimi <mmaz13841384@gmail.com>
2025-05-11 20:23:55 +0330

    added Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,9 @@
-
 function Footer() {
   return (
-    <div className="bg-gradient-to-tr from-[#121212] to-[#3b3b3b] h-20 relative top-39">
-      Footer
+    <div className="bg-gradient-to-tr from-[#000000] to-[#0c0c0c] text-[#929292] mt-10 flex items-center justify-center flex-col">  
+    <h1 className="font-mono">Code Haven</h1>
+    <h1 className="font-mono">2024</h1>
+    <h1 className="font-mono">All Of My Programing Is Here</h1>
     </div>
   );
 }
